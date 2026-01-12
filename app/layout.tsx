40028458b076e646b4866bfeace6c22d169d9534
Nick <nikolancemoro@gmail.com>
2026-01-12 22:03:37 +0000

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,7 @@
 import type { Metadata } from "next";
-//import { Geist, Geist_Mono } from "next/font/google";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import AppHeader from "@/components/AppHeader";
-
-/*
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-*/
 
 const inter = Inter({
   subsets: ["latin"],
